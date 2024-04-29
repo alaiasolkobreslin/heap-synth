732d32ffd6cac75ff87107cb6068f5f3f23716de
Alaia Solko-Breslin <ajs644@cornell.edu>
2024-04-29 12:24:38 -0400

    some synthesis fixes

--- src/synthesize.ml
+++ src/synthesize.ml
@@ -8,38 +8,44 @@ let is_invert (r: rule) : bool =
   | REmp -> true
   | _ -> false
 
-let filter_comm (x: rule_result option) =
-  match x with
-  | None -> None
-  | Some rule_result -> failwith "unimplemented"
+let filter_comm (x: rule_result option) = x
 
+let combine_cmds (cmd_lst: cmd list) : cmd =
+  let rec combine_cmds' (cmd_lst: cmd list) (acc : cmd) =
+    match cmd_lst with
+    | [] -> acc
+    | cmd::cmds -> combine_cmds' cmds (CSeq (acc, cmd))
+  in
+  combine_cmds' cmd_lst CSkip
 
-let rec solve_subgoals goals k =
+let continuation (results: rule_result list) : cmd =
+  let cmds = List.map (fun r -> r.producer) results in
+  combine_cmds cmds
+
+let rec solve_subgoals goals (k: rule_result list -> cmd) =
   let cs = List.filter_map (fun g -> synthesize g all_rules) goals in
-  if List.length cs = List.length goals then Some (k cs) else None
+  if List.length cs = List.length goals then Some (combine_cmds cs) else None
 
-and try_alts derivs r rs g =
+and try_alts (derivs: goal list) (r: rule) (rs: rule list) (g: goal) : cmd option =
   match derivs with
   | [] -> if is_invert r then None else with_rules rs g
-  | (goals, k)::derivs' ->
-    begin
-      match solve_subgoals goals k with
+  | goal :: derivs' ->
+      match solve_subgoals [goal] continuation with
       | None -> try_alts derivs' r rs g
       | Some c -> Some c
-    end
 
-and with_rules (rs: rule list) (g: goal) =
+and with_rules (rs: rule list) (g: goal) : cmd option =
   match rs with
   | [] -> None
   | r::rs' ->
     begin
       match filter_comm (apply_rule r g) with
       | None -> with_rules rs' g
-      | Some derivs -> try_alts derivs r rs' g
+      | Some derivs -> try_alts derivs.subgoals r rs' g
     end
 
-and synthesize (g: goal) (rules:rule list) =
+and synthesize (g: goal) (rules:rule list): cmd option =
   with_rules rules g
 
-let heap_synth (g: goal) =
+let heap_synth (g: goal) : cmd option =
   synthesize g all_rules
